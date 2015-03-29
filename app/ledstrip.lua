eacae710099f1aab751ece4c2ec3a610e191e2c4
Naren Vasanad <naren.vasanad@berkeley.edu>
2015-03-29 12:14:40 -0700

    Changed number of leds in strip to 50

--- app/ledstrip.lua
+++ app/ledstrip.lua
@@ -13,7 +13,8 @@ require "cord" -- scheduler / fiber library
 storm.io.set_mode(storm.io.OUTPUT, storm.io.D2)
 storm.io.set_mode(storm.io.OUTPUT, storm.io.D3)
 
-strip = storm.n.led_init(5, 0x10000, 0x1000)
+-- SCLK: D2 and SDO: D3
+strip = storm.n.led_init(50, 0x10000, 0x1000)
 storm.n.led_set(strip, 0, 31, 20, 0)
 storm.n.led_set(strip, 2, 31, 10, 0)
 storm.n.led_show(strip)
